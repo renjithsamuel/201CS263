98bd76cbd42cbdcb6ab84add8817d2d068996032
renjithsamuel <ranjithsamuelking@gmail.com>
2023-07-26 12:17:38 +0530

    prototype 2.0.5

--- frontend/trainreservation/src/components/SingleTrainCard/SingleTrainCard.jsx
+++ frontend/trainreservation/src/components/SingleTrainCard/SingleTrainCard.jsx
@@ -1,4 +1,5 @@
 import './SingleTrainCard.css'
+import trainIcon from '../../assets/train-light.svg'
 
 function SingleTrainCard({train}) {
 
@@ -12,29 +13,80 @@ function SingleTrainCard({train}) {
                   {(train && train.trainNumber)?train.trainNumber : ''}
                 </div>
             </div>
-            <div className="trainCardLeft"> 
-                <div className="trainDepartureTime">
-                    {/* {(train && train.departureTime)?train.departureTime : ''} */}
-                </div>
-                <div className="trainDelay">
-                    {(train && train.delayedBy)?train.delayedBy : ''}
-                </div>
-            </div>
-            <div className="trainCardRight">
-                <div className="seatsAvailable">
-                    <div className="sleeperCount">
-                        {(train && train.seatsAvailable)?train.seatsAvailable.sleeper : ''}
+            <div className="trainCardBottom">
+                <div className="trainCardLeft">
+                    <div className="trainIcon">
+                        <img src={trainIcon} alt="train" height={100}  width={100}/>
                     </div>
-                    <div className="acCount">
-                    {(train && train.seatsAvailable)?train.seatsAvailable.AC : ''}
+                    <div className="trainTimings">
+                        <div className="trainDepartureTime">
+                            <div className="trainDepartureName">
+                                Departure at
+                            </div>
+                            <div className="trainDepartureDisplay">
+                            {train.departureTime.Hours} : { train.departureTime.Minutes}
+                            </div>
+                        </div>
+                        <div className="trainDelay">
+                            <div className="trainDelayName">
+                                Delayed by
+                            </div>
+                            <div className="trainDelayDisplay">
+                                 {(train && train.delayedBy)?train.delayedBy : ''} mins
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="price">
-                     <div className="sleeperPrice">
-                        {(train && train.price)?train.price.sleeper : ''}
+                <div className="trainCardRight">
+                    <div className="seatsAvailable">
+                        <div className="seatsAvailableName">
+                            Seats Available
+                        </div>
+                        <div className="seatsOverAllDisplay">
+                            <div className="sleeperCount">
+                                <div className="sleeperCountName">
+                                    Sleeper
+                                </div>
+                                <div className="sleeperCountDisplay">
+                                    {(train && train.seatsAvailable)?train.seatsAvailable.sleeper : ''}
+                                </div>
+                            </div>
+                            <div className="acCount">
+                                <div className="acCountName">
+                                    AC
+                                </div>
+                                <div className="acCountDisplay">
+                                {(train && train.seatsAvailable)?train.seatsAvailable.AC : ''}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="acPrice">
-                         {(train && train.price)?train.price.AC : ''}
+                    <div className="price">
+
+                        <div className="priceName">
+                            Prices
+                        </div>
+
+                         <div className="overAllPrices">
+                             <div className="sleeperPrice">
+                                <div className="sleeperPriceName">
+                                    Sleeper
+                                </div>
+                                <div className="sleeperPriceDisplay">
+                                {(train && train.price)?train.price.sleeper : ''}
+                                </div>
+                                                     </div>
+                             
+                                <div className="acPrice">
+                                        <div className="acPriceName">
+                                            AC
+                                        </div>
+                                        <div className="acPriceDisplay">
+                                        {(train && train.price)?train.price.AC : ''}
+                                        </div>
+                                </div>
+                         </div>
+                         
                     </div>
                 </div>
             </div>
